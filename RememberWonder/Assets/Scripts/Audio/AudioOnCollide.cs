4a030029b71f0ab6773ccdf1feb5a1362b6dfd01
Patrick <prm5983@rit.edu>
2023-02-17 16:51:22 -0500

    Added debug info for audio on collide
    ...since it's kind of unintuitive how much impulse any given collision's going to have in numbers.

--- RememberWonder/Assets/Scripts/Audio/AudioOnCollide.cs
+++ RememberWonder/Assets/Scripts/Audio/AudioOnCollide.cs
@@ -10,9 +10,23 @@ public class AudioOnCollide : MonoBehaviour
     [Tooltip("How much change in momentum the collision has to have before playing any audio." +
         "See https://wikipedia.org/wiki/Impulse_%28physics%29.")]
     [SerializeField][Min(0)] float impulseThreshold = 1;
+#if UNITY_EDITOR
+    [Header("Editor Only")]
+    [SerializeField] bool printCollisionResults;
+#endif
 
     private void OnCollisionEnter(Collision collision)
     {
+#if UNITY_EDITOR
+        if (printCollisionResults)
+        {
+            print($"{name} AudioOnCollide: <color=#F85>( {collision.impulse.magnitude} >= {impulseThreshold} )^2 </color>" +
+                $"== <color=#58F>{collision.impulse.sqrMagnitude} >= {impulseThreshold * impulseThreshold} </color>" +
+                $"== <color={(collision.impulse.sqrMagnitude >= impulseThreshold * impulseThreshold ? "#FFF" : "#666")}>" +
+                $"{collision.impulse.sqrMagnitude >= impulseThreshold * impulseThreshold}</color>");
+        }
+#endif
+
         if (collision.impulse.sqrMagnitude >= impulseThreshold * impulseThreshold)
         {
             AudioHub.Inst.Play(audioToPlay, audioSettings, collision.GetContact(0).point);
