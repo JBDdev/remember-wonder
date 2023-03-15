92ed3490f1a984c4873759a44748112066a21fb2
JBDdev <brodydavison4@gmail.com>
2023-03-15 01:17:37 -0400

    idle animation for elephant on title screen

--- RememberWonder/Assets/Scripts/EndLevelTrigger.cs
+++ RememberWonder/Assets/Scripts/EndLevelTrigger.cs
@@ -22,18 +22,32 @@ public class EndLevelTrigger : MonoBehaviour
     {
         if (!triggererTags.Contains(col.gameObject.tag)) return;
 
+        //Run the call to clear the UI
+        //Run the call to stop player input
+
+        //Run the call to pull up end screen
+        //Spawn in the greyed out puzzle pieces (or just have them pre-added in advance)
+        //Loop thru and fill in each piece + play audio
+        //Elephant backflip?
+        //Assign Press A to Continue and make that prompt show up on screen
+
+        
+    }
+
+    void OnPressContinue(UnityEngine.InputSystem.InputAction.CallbackContext ctx) 
+    {
         /*if (moteUI.CollectedCount >= 10)
         {*/
-            if (string.IsNullOrWhiteSpace(scene))
-            {
-                SceneManager.LoadScene(sceneIndex >= 0
-                    ? sceneIndex
-                    : SceneManager.GetActiveScene().buildIndex + -sceneIndex);
-            }
-            else
-            {
-                SceneManager.LoadScene(scene);
-            }
+        if (string.IsNullOrWhiteSpace(scene))
+        {
+            SceneManager.LoadScene(sceneIndex >= 0
+                ? sceneIndex
+                : SceneManager.GetActiveScene().buildIndex + -sceneIndex);
+        }
+        else
+        {
+            SceneManager.LoadScene(scene);
+        }
         /*}
 
         else Debug.Log("Player only has " + moteUI.GetComponent<MoteUIController>().CollectedCount + " motes.");*/
