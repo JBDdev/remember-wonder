e1ef5e8806b82221f7c90a064090e7f2e39d0194
Patrick <prm5983@rit.edu>
2023-04-26 17:14:54 -0400

    Fix unintentional credits skip, add fast forward

--- RememberWonder/Assets/Scripts/User Interfaces/EndLevelSequence.cs
+++ RememberWonder/Assets/Scripts/User Interfaces/EndLevelSequence.cs
@@ -5,23 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class EndLevelSequence : MonoBehaviour
 {
-    [SerializeField] GameObject creditsText;
+    [SerializeField] RectTransform creditsTextObj;
     [SerializeField] float scrollSpeed;
+    [SerializeField] float fasterScrollSpeed;
+
     // Start is called before the first frame update
     void Start()
     {
-        InputHub.Inst.Gameplay.Jump.performed += LoadTitleScreen;
+        InputHub.Inst.UI.Select.performed += LoadTitleScreen;
     }
 
     // Update is called once per frame
     void Update()
     {
-        creditsText.transform.position += new Vector3(0, scrollSpeed, 0) * Time.deltaTime;
+        //Scroll the credits up faster if back button is held
+        creditsTextObj.position += Vector3.up
+            * (InputHub.Inst.UI.Back.IsPressed() ? fasterScrollSpeed : scrollSpeed)
+            * Time.deltaTime;
     }
 
     void LoadTitleScreen(UnityEngine.InputSystem.InputAction.CallbackContext ctx) 
     {
-        InputHub.Inst.Gameplay.Jump.performed -= LoadTitleScreen;
+        InputHub.Inst.UI.Select.performed -= LoadTitleScreen;
         SceneManager.LoadScene(0);
     }
 }
